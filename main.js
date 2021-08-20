8e676029ccd94584ae513dd1e7511b29dba620c3
cyberck <dushaev2017@gmail.com>
2021-08-20 11:58:33 -0700

    JS urok-1, domashka

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+let imya = prompt('Vvedite imya'),
+    vozrost = prompt('Skolko let'),
+    primOne = +prompt('6 + 3 = ?'),
+    primTwo = +prompt('20 - 5 = ?'),
+    primThree = +prompt('15 * 2 = ?'),
+    primFour = +prompt('8 / 4 = ?'),
+    primFive = +prompt('10 % 3 = ?')
+alert('!!!!!Zaydite v koncole!!!!!')
+
+
+    console.log(imya);
+    console.log(vozrost);
+    console.log('Primer 1: 6 + 3 = 9 Vash otvet ' + primOne);
+    console.log('Primer 2: 20 - 5 = 15 Vash otvet ' + primTwo);
+    console.log('Primer 3: 15 * 2 = 30 Vash otvet ' + primThree);
+    console.log('Primer 4: 8 / 4 = 2 Vash otvet ' + primFour);
+    console.log('Primer 5: 10 % 3 = 1 Vash otvet ' + primFive);
+
+
+
+
+
+
